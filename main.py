f280b18c443d0de6896fe178ac36664db7c440e1
brilliantik <aauraimov@gmail.com>
2022-10-06 20:11:42 +0300

    1) Class Config удален из main.py 2) Class Config импортируется из ImageCreate.py (ImageCreate.Config())

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import xml.etree.ElementTree as ET
 from PIL import Image
+
+import ImageCreate
 from grid import *
 from FemFrameTool import read_fun
 from IntegratorST import integral_jacob, integrate_by_st
-from ImageCreate import create_image
+from ImageCreate import create_image, Config
 import sys
+
+
+grid = None
+field = None
+jac = None
+pic = None
+clr = []
+cfg: Config
 
 
 class Color:
@@ -18,30 +28,6 @@ class Color:
 		f = int(d[0])
 		t = int(d[1])
 		self.ft = (f, t)
-
-
-class Config:
-	def __init__(self, pixel, path_file_names, path_file_names_for_save, path_folder):
-		self.pixel_size = pixel
-		self.path_default_wells_dat = path_file_names[0]
-		self.path_result_wll = path_file_names[1]
-		self.path_default_con = path_file_names[2]
-		self.path_SLsReg_auto_txt = path_file_names[3]
-		self.path_default_net = path_file_names[4]
-		self.path_result_fun = path_file_names[5]
-		self.path_save_picture_SL = path_file_names_for_save[0]
-		self.path_save_picture_ST = path_file_names_for_save[1]
-		self.path_save_color_and_conn_info = path_file_names_for_save[2]
-		self.path_save_st_Well_From_To_colorRGB_value = path_file_names_for_save[3]
-		self.path_folder_save = path_folder
-
-
-grid = None
-field = None
-jac = None
-pic = None
-clr = []
-cfg: Config
 
 
 def read_xml_config():
@@ -58,7 +44,7 @@ def read_xml_config():
 	path_file_names_for_save = [
 		path_save + file_names_for_save.text for file_names_for_save in
 		root.find('file_names_for_save').findall('file_name_save')]
-	cfg = Config(pixel_size, path_file_names, path_file_names_for_save, path_save)
+	cfg = ImageCreate.Config(pixel_size, path_file_names, path_file_names_for_save, path_save)
 
 
 def read_pic(path):
